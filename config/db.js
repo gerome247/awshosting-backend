4e98dcb246cf2f4126bbc90486447187b33fcccb
gerome247 <gerome247@gmail.com>
2025-03-05 14:01:05 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rewtc.mongodb.net/TodoList?retryWrites=true&w=majority&appName=Cluster0`);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("Error connecting to db: ", error);
